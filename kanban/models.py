652842f61ee94c92094d5d22050ea617eb66d293
Macelo <maceloaugusto066@gmail.com>
2025-04-25 17:30:02 -0300

    algumas alterações

--- kanban/models.py
+++ kanban/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+
+    create = models.DateField('CreateAt', auto_now_add=True)
+    change = models.DateField('ChangesTo', auto_now=True)
+    Status = models.BooleanField('Status', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Task(Base):
+
+    STATUS_CHOICES = [
+        ('NI', 'Não iniciado'),
+        ('EA', 'Em Andamento'),
+        ('CL', 'Concluido')
+    ]
+
+    title = models.CharField('Title', max_length=120)
+    description = models.CharField('Description', max_length=220)
+    task_status = models.CharField('Task Status', max_length=12, choices=STATUS_CHOICES)
+    task_conclusion = models.DateField('Ended', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Tarefa'
+        verbose_name_plural = 'Tarefas'
+
+    def __str__(self):
+        return self.title
--- kanban/views.py
+++ kanban/views.py
@@ -1,3 +1,24 @@
+from django.views.generic import FormView, TemplateView, View
 from django.shortcuts import render
+from .forms import TaskForm
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+
+class IndexView(FormView):
+
+    template_name = 'index.html'
+    form_class = TaskForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        print(self.request.POST)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return render(self.request, self.template_name, {
+            "form": form,
+            "messagem_erro": 'Por favor corriga os erros.'
+        })
+
